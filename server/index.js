06fc3df3417eae78d90f2bc0538bcfdb594fcd51
Zachary <zachary.cimorelli09@gmail.com>
2025-02-20 15:18:33 -0500

    Refactor server to use Express framework for handling requests

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-const http = require('http')
+const express = require('express');
 const PORT = 8000
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World!!\n');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-server.listen(PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
 
