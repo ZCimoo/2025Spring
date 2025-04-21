0240fdf2438d07641d2dae3eea0747cf11c1ef2a
Zachary <zachary.cimorelli09@gmail.com>
2025-04-21 14:47:15 -0400

    Attached user model to db

--- server/models/users.js
+++ server/models/users.js
@@ -1,55 +1,160 @@
 const data = require("../data/users.json");
 const { CustomError, statusCodes } = require("./errors");
-async function getAll() {
-  return data;
+const { connect } = require("./supabase");
+
+const TABLE_NAME = "users";
+
+const BaseQuery = () =>
+  connect()
+    .from(TABLE_NAME)
+    .select("*, product_reviews(average_rating:rating.avg())", {
+      count: "estimated",
+    });
+//.select('*')
+
+const isAdmin = true;
+
+async function getAll(limit = 30, offset = 0, sort = "id", order = "desc") {
+  const list = await BaseQuery()
+    .order(sort, { ascending: order === "asc" })
+    .range(offset, offset + limit - 1); // 0 based index but range is inclusive
+  if (list.error) {
+    throw list.error;
+  }
+  return {
+    items: list.data,
+    total: list.count,
+  };
 }
 
 async function get(id) {
-  const item = data.items.find((item) => item.id == id);
-  if (!item) {
+  const { data: item, error } = await connect()
+    .from(TABLE_NAME)
+    .select("*, product_reviews(*)")
+    .eq("id", id);
+  if (!item.length) {
     throw new CustomError("Item not found", statusCodes.NOT_FOUND);
+  }
+  if (error) {
+    throw error;
   }
   return item;
 }
 
+async function search(
+  query,
+  limit = 30,
+  offset = 0,
+  sort = "id",
+  order = "desc"
+) {
+  const {
+    data: items,
+    error,
+    count,
+  } = await BaseQuery()
+    .or(
+      `firstName.ilike.%${query}%,lastName.ilike.%${query}%,phone.ilike.%{query}%`
+    )
+    .order(sort, { ascending: order === "asc" })
+    .range(offset, offset + limit - 1);
+  if (error) {
+    throw error;
+  }
+  return {
+    items,
+    total: count,
+  };
+}
+
 async function create(item) {
   if (!isAdmin) {
-    throw new CustomError(
-      "Sorry, you are not authorized to create a new item.",
+    throw CustomError(
+      "Sorry, you are not authorized to create a new item",
       statusCodes.UNAUTHORIZED
     );
   }
-  const newItem = {
-    id: data.items.length + 1,
-    ...item,
-  };
-  data.items.push(newItem);
+  const { data: newItem, error } = await connect()
+    .from(TABLE_NAME)
+    .insert(item)
+    .select("*");
+  if (error) {
+    throw error;
+  }
   return newItem;
 }
 
 async function update(id, item) {
-  const index = data.items.findIndex((item) => item.id == id);
-  if (index === -1) return null;
-  const updatedItem = {
-    ...data[index], //takes every property of the object and copies all properties from original object and adds new properties
-    ...item,
-  };
-  data[index] = updatedItem; //stores new item at that location
+  if (!isAdmin) {
+    throw CustomError(
+      "Sorry, you are not authorized to update this item",
+      statusCodes.UNAUTHORIZED
+    );
+  }
+  const { data: updatedItem, error } = await connect()
+    .from(TABLE_NAME)
+    .update(item)
+    .eq("id", id)
+    .select("*");
+  if (error) {
+    throw error;
+  }
   return updatedItem;
 }
 
 async function remove(id) {
-  const index = data.items.findIndex((item) => item.id == id);
-  if (index === -1) return null;
-  const deletedItem = data[index];
-  data.items.splice(index, 1); //removes the item from the array
+  if (!isAdmin) {
+    throw CustomError(
+      "Sorry, you are not authorized to delete this item",
+      statusCodes.UNAUTHORIZED
+    );
+  }
+  const { data: deletedItem, error } = await connect()
+    .from(TABLE_NAME)
+    .delete()
+    .eq("id", id);
+  if (error) {
+    throw error;
+  }
   return deletedItem;
+}
+
+async function seed() {
+  for (const item of data.items) {
+    const insert = mapToDB(item);
+    const { data: newItem, error } = await connect()
+      .from(TABLE_NAME)
+      .insert(insert)
+      .select("*");
+    if (error) {
+      throw error;
+    }
+  }
+  return { message: "Seeded successfully" };
+}
+
+function mapToDB(item) {
+  return {
+    //id: item.id,
+    firstName: item.firstName,
+    lastName: item.lastName,
+    email: item.email,
+    phone: item.phone,
+    age: item.age,
+    gender: item.gender,
+    birthDate: item.birthDate,
+    image: item.image,
+    university: item.university,
+    role: item.role,
+  };
 }
 
 module.exports = {
   getAll,
   get,
+  search,
   create,
   update,
   remove,
+  seed,
 };
